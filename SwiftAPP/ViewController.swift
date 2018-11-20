136accd4827c330cd680d37e4da872698df452e1
Marcelo <marcelo95326@icloud.com>
2018-11-20 17:59:37 -0500

    Initial Commit

---
+++ SwiftAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftAPP
+//
+//  Created by Marcelo Villalba on 11/20/18.
+//  Copyright © 2018 Marcelo Villalba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
